54133aeb21ee0e1cf0bdd1b650fdf82351ff9e30
Llorch <skylakebeta@gmail.com>
2017-05-13 23:23:16 -0700

    Fix frontend route, page refresh

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+
+app.get('*', function(req, res){
+	res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 app.listen(PORT, function(){
 	console.log('Server listening on PORT: ', PORT);
